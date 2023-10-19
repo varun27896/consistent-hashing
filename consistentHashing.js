e7e07c72d1ce5f5ed4302dfff37ffe0abc68209d
varun27896 <varun27896@gmail.com>
2023-10-18 22:37:29 -0500

    adds docblocks

--- consistentHashing.js
+++ consistentHashing.js
@@ -6,6 +6,14 @@ class ConsistentHashing {
         this.sortedOrder = [];
     }
 
+    /**
+     * Adds a new node to the hash ring.
+     *
+     * @param {string} nodeId - Unique identifier for the node to be added.
+     * @returns {string} The hash of the added node.
+     *
+     * @memberOf ConsistentHashing
+     */
     addNode(nodeId) {
         let hashId = this.hashFunction(nodeId);
 
@@ -16,6 +24,14 @@ class ConsistentHashing {
         return hashId;
     }
 
+    /**
+     * Removes a node from the hash ring by its nodeId.
+     *
+     * @param {string} nodeId - Unique identifier for the node to be removed.
+     * @returns {boolean} True if the node was found and removed, false otherwise.
+     *
+     * @memberOf ConsistentHashing
+     */
     removeNode(nodeId) {
         let foundNode = false;
         let nodeHash = null;
@@ -41,14 +57,51 @@ class ConsistentHashing {
         return true;
     }
 
+    /**
+     * Retrieves the node responsible for the given key.
+     *
+     * @param {string} key - The key for which to find the responsible node.
+     * @returns {string} The node responsible for handling the provided key.
+     *
+     * @memberOf ConsistentHashing
+     */
     getNodeForKey(key) {
+        if (this.ring.size === 0) {
+            throw new Error('No nodes available in the ring');
+        }
 
+        const keyHash = this.hashFunction(key);
+
+        for (const nodeHash of this.sortedOrder) {
+            if (nodeHash.localeCompare(keyHash) >= 0) {
+                return this.ring.get(nodeHash);
+            }
+        }
+
+        // If we've gone through the entire ring and the keyHash is greater than any existing node hash,
+        // we loop back to the first node in the ring (circular nature of the consistent hash ring)
+        return this.ring.get(this.sortedOrder[0]);
     }
 
+    /**
+     * Generates a SHA-1 hash for the provided input.
+     *
+     * @param {string} input - The input for which to generate the hash.
+     * @returns {string} The generated SHA-1 hash.
+     *
+     * @memberOf ConsistentHashing
+     */
     hashFunction(input) {
         return crypto.createHash('sha1').update(input).digest('hex');
     }
 
+    /**
+     * Retrieves a list of all nodes in the hash ring.
+     *
+     * @returns {Array} An array of all nodes in the ring.
+     *
+     * @memberOf ConsistentHashing
+     */
     getNodes() {
         let nodes = [];
 
@@ -59,6 +112,7 @@ class ConsistentHashing {
         return nodes;
     }
 
+    
     rebalance() {
 
     }
